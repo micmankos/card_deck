bd8480d65f994d915fe7f8bd85a501d994bdf053
Michael Mankos <micmankos@gmail.com>
2020-04-30 16:54:26 -0400

    passing tests

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -4,6 +4,11 @@ Tests functionality of deck and card modules
 import pytest
 import sg_deck
 
+CARDS = ['King of Diamonds', 'Queen of Diamonds', 'Jack of Diamonds', '10 of Diamonds', '9 of Diamonds', '8 of Diamonds', '7 of Diamonds', '6 of Diamonds', '5 of Diamonds', '4 of Diamonds', '3 of Diamonds', '2 of Diamonds', 'Ace of Diamonds', \
+            'King of Clubs', 'Queen of Clubs', 'Jack of Clubs', '10 of Clubs', '9 of Clubs', '8 of Clubs', '7 of Clubs', '6 of Clubs', '5 of Clubs', '4 of Clubs', '3 of Clubs', '2 of Clubs', 'Ace of Clubs', \
+            'King of Spades', 'Queen of Spades', 'Jack of Spades', '10 of Spades', '9 of Spades', '8 of Spades', '7 of Spades', '6 of Spades', '5 of Spades', '4 of Spades', '3 of Spades', '2 of Spades', 'Ace of Spades', \
+            'King of Hearts', 'Queen of Hearts', 'Jack of Hearts', '10 of Hearts', '9 of Hearts', '8 of Hearts', '7 of Hearts', '6 of Hearts', '5 of Hearts', '4 of Hearts', '3 of Hearts', '2 of Hearts', 'Ace of Hearts']
+
 class TestDeck:
     
     def test_can_deal_52_cards(self):
@@ -125,9 +130,44 @@ class TestDeck:
             assert(isinstance(c.name, str))
             assert(isinstance(c.rank, str))
             assert(isinstance(c.suit, str))
+
+    def test_card_name_matches_rank_and_suit(self):
+        deck = sg_deck.Deck()
+        while not deck.is_empty():
+            c = deck.deal_card()
+            rank_and_suit = f"{c.rank} of {c.suit}"
+            assert(c.name == rank_and_suit)
+
+        deck.shuffle()
+        while not deck.is_empty():
+            c = deck.deal_card()
+            rank_and_suit = f"{c.rank} of {c.suit}"
+            assert(c.name == rank_and_suit)
         
     def test_init_deck_deals_all_poker_cards(self):
-        assert 0
+        card_set = set()
+        for c in CARDS:
+            card_set.add(c)
+            
+        deck = sg_deck.Deck()
+        while not deck.is_empty():
+            c = deck.deal_card()
+            assert(c.name in card_set)
+            card_set.remove(c.name)
+
+        assert(len(card_set) == 0)
 
     def test_shuffled_deck_deals_all_poker_cards(self):
-        assert 0
+        card_set = set()
+        for c in CARDS:
+            card_set.add(c)
+            
+        deck = sg_deck.Deck()
+        deck.shuffle()
+        
+        while not deck.is_empty():
+            c = deck.deal_card()
+            assert(c.name in card_set)
+            card_set.remove(c.name)
+
+        assert(len(card_set) == 0)
