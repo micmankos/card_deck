3c15c7285de6dc9de72412b94b5e77f4ee1441b3
Michael Mankos <micmankos@gmail.com>
2020-04-30 15:07:32 -0400

    added card object

--- src/Card.py
+++ src/Card.py
@@ -1,3 +1,12 @@
 '''
 Card belonging to poker style deck
 '''
+class Card:
+    def __init__(self, rank, suit):
+        '''
+        Poker Card where suit and rank are represented as strings
+        '''
+        self.suit = suit
+        self.rank = rank
+        self.name = f"{rank} of {suit}"
+        
