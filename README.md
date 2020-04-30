8e816a534ffb362a122477ecbec8767475591ea9
Michael Mankos <micmankos@gmail.com>
2020-04-30 13:26:25 -0400

    added some tests - all failing

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ Author: Michael Mankos
 This module implements a standard Poker-style deck of cards which can be used to implement a card game. 
 
 ## Dev Environment
+Built with Python 3.6.5, pytest, and pylint. 
+
+From the top-level directory of the repo, run tests with
+```
+pytest
+```
 
 ## Usage
+
 
 ## Build
---
+++ test/test_deck.py
@@ -0,0 +1,27 @@
+class TestDeck:
+    def test_get_53rd_card(self):
+        assert 0
+
+    def test_can_deal_52_cards(self):
+        assert 0
+
+    def test_deal_card_reduces_deck_cnt(self):
+        assert 0
+
+    def test_shuffled_deck_has_52_cards(self):
+        assert 0
+
+    def test_shuffle_has_valid_cards(self):
+        assert 0
+
+    def test_deck_is_empty_on_53rd_card_dealt(self):
+        assert 0
+
+    def test_deck_has_cards_for_52_cards_dealt(self):
+        assert 0
+
+    def test_init_deck_has_valid_cards(self):
+        assert 0
+
+    def test_init_deck_has_52_cards(self):
+        assert 0
